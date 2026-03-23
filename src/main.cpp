fe5b674d443653e498c56610624a51d3833dcd93
Dalton Charleston <ddcharleston94@gmail.com>
2026-03-23 16:58:48 -0500

    Adding Raylib and opening window

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <raylib.h>
+
+bool isRunning = true;
 
 int main()
 {
-	std::cout << "Hello\n";
 
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+	InitWindow(1280,720, "Terriria Clone");
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+
+		DrawText("First Window Complete",(1280/2),(720/2),36,BLACK);
+
+		DrawRectangle(50,50,100,100,{255,0,0,127});
+		DrawRectangle(75,75,100,100,{0,255,0,127});
+
+
+
+		EndDrawing();
+	}
+
+	CloseWindow();
 	return 0;
 }
